4ce585ccfeefc3c250e02d6e3ada98ecbe459818
clelann <cedric.lelann@laposte.net>
2018-09-03 14:51:56 +0200

    calculatrice

---
+++ calculatrice.ps1
@@ -0,0 +1,14 @@
+﻿echo "Calculatrice 1.0"
+[int] $1 = read-host "Saisir le premier nombre"
+[string] $op = read-host "Veuillez choisir l'operation entre ces choix: + - / *"
+[int] $2 = Read-Host "Saisir le deuxieme nombre"
+
+
+switch($op) {
+"+" { $1 + $2 }
+"-" { $1 - $2 }
+"/" { $1 / $2 }
+"*" { $1 * $2 }
+default {$res = "Pas compris"}
+}
+
